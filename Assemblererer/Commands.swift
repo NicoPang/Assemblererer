291bd1aa88ba4b214af1425e9116b3613ad49028
Nicko <n17pang@gmail.com>
2019-03-27 12:31:20 -0400

    added support and commands

---
+++ Assemblererer/Commands.swift
@@ -0,0 +1,13 @@
+//
+//  Commands.swift
+//  Assemblererer
+//
+//  Created by Nick Pang on 3/27/19.
+//  Copyright © 2019 Nick's Projects. All rights reserved.
+//
+
+import Foundation
+
+enum Command: Int {
+    case halt = 0, clrr, clrx, clrm, clrb, movir, movrr, movrm, movmr, movxr, movar, movb, addir, addrr, addmr, addxr, subir, subrr, submr, subxr, mulir, mulrr, mulmr, mulxr, divir, divrr, divmr, divxr, jmp, sojz, sojnz, aojz, aojnz, cmpir, cmprr, cmpmr, jmpn, jmpz, jmpp, jsr, ret, push, pop, stackc, outci, outcr, outcx, outcb, readi, printi, readc, readln, brk, movrx, movxx, outs, nop, jmpne
+}
---
+++ Assemblererer/Support.swift
@@ -0,0 +1,49 @@
+//
+//  Support.swift
+//  Assemblererer
+//
+//  Created by Nick Pang on 3/27/19.
+//  Copyright © 2019 Nick's Projects. All rights reserved.
+//
+
+import Foundation
+
+func fit(s: String, size: Int, replacement: Character, right: Bool) -> String {
+    let sSize = s.count
+    if sSize == size {
+        return s
+    }
+    if (size < sSize) {
+        let index = s.index(s.startIndex, offsetBy: size)
+        return String(s[...index])
+    }
+    var addon = ""
+    let emptySpace = size - sSize;
+    for _ in 0..<emptySpace {
+        addon += String(replacement)
+    }
+    return right ? s + addon : addon + s
+}
+
+
+func characterToUnivodeValue(_ c: Character) -> Int{
+    return Int(c.unicodeScalars[c.unicodeScalars.startIndex].value)
+}
+
+func unicodeValueToCharacter(_ n: Int) -> Character{
+    return Character(UnicodeScalar(n)!)
+}
+
+func stringToUnicodeValues(_ s: String) -> [Int] {
+    let characters = Array(s)
+    return characters.map{Int($0.unicodeScalars[$0.unicodeScalars.startIndex].value)}
+}
+
+func splitStringIntoParts(_ expression: String) -> [String] {
+    return expression.components(separatedBy: " ")
+}
+func splitStringIntoLines(_ expression: String) -> [String] {
+    return expression.components(separatedBy: "\n")
+}
+
+
