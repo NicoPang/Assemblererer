e2a7d01b7eab66cbb5d10fd4e3940e1d17d104a3
Nicko <n17pang@gmail.com>
2019-05-20 13:36:12 -0400

    added debugger

---
+++ Assemblererer/Debugger.swift
@@ -0,0 +1,21 @@
+//
+//  Debugger.swift
+//  Assemblererer
+//
+//  Created by Nick Pang on 5/20/19.
+//  Copyright © 2019 Nick's Projects. All rights reserved.
+//
+
+import Foundation
+
+enum StatusFlag {
+    case S, G, Exit
+}
+ 
+
+extension Assembler {
+    
+    func debug() {
+        print("Sdb (\(self.pvm.rPC))")
+    }
+}
